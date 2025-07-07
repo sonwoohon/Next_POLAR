657fa60c946ed87a0c63086fc22d7bcf4c071c1a
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-07-07 10:28:29 +0900

    feat : BE 클린 아키텍처 구조 세팅

---
+++ app/(backend)/seniors/helps/applications/usecases/senior-help-usecases.ts
@@ -0,0 +1,11 @@
+// Senior 도움 생성, 수정, 연결, 완료, help 조건부 삭제 관련 유스케이스
+// 이 파일에는 Senior 사용자의 도움 생성, 수정, 연결, 완료, help 조건부 삭제 관련 비즈니스 로직이 구현됩니다.
+
+// 예시:
+// - CreateHelpUseCase: 도움 생성
+// - UpdateHelpUseCase: 도움 수정
+// - ConnectHelpUseCase: 도움 연결(junior가 help에 지원하면 senior가 수락하여 연결)
+// - CompleteHelpUseCase: 도움 완료(senior가 완료 버튼을 누르고, junior가 인증 코드를 입력하면 완료)
+// - DeleteHelpUseCase: help 조건부 삭제
+
+// 유스케이스는 비즈니스 로직을 구현하며, 엔티티를 조작하고 비즈니스 규칙을 적용합니다.
