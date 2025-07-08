997a03fbf199aa82271e51d0c1ec417f164f0a60
HarenKei <seungmin1266@gmail.com>
2025-07-08 17:36:12 +0900

    Feat: Add CommonHelpEntity
    헬프 시스템의 공통 도메인 엔터티를 정의했습니다.
    파일명을 CommonHelp -> CommonHelpEntity로 변경해서
    클래스명과 파일명을 일치시켰습니다.

---
+++ backend/helps/domains/entities/CommonHelpEntity.ts
@@ -0,0 +1,36 @@
+// helps 리스트와 상세를 조회 하는 엔티티
+// 이 파일에는 helps 리스트와 상세를 조회 하는 도메인 엔티티들이 정의됩니다.
+
+// 예시:
+// - Help: helps 리스트 정보
+// - HelpDetail: help의 상세 데이터 정보
+
+// 도메인 엔티티는 비즈니스 규칙을 포함하며, 외부 의존성이 없는 순수한 객체여야 합니다.
+
+export class CommonHelpEntity {
+  constructor(
+    public id: number,
+    public seniorId: number,
+    public title: string,
+    public startDate: Date,
+    public endDate: Date,
+    public category: number,
+    public content: string,
+    public status: string,
+    public createdAt: Date
+  ) { }
+
+  toJSON() {
+    return {
+      id: this.id,
+      seniorId: this.seniorId,
+      title: this.title,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      category: this.category,
+      content: this.content,
+      status: this.status,
+      createdAt: this.createdAt
+    };
+  }
+}
