40e7c689dde88f1e81dd0c536e7360a3adeec090
HarenKei <seungmin1266@gmail.com>
2025-07-14 11:22:40 +0900

    Rename: Remname sign up route directory
    signup 디렉토리 이름을 sign-up으로 변경하여 라우팅 컨벤션 준수

---
+++ app/(pola)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import styles from "./_styles/signup.module.css";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+const SignupPage: React.FC = () => {
+  return <div className={styles.signupContainer}></div>;
+};
+
+export default SignupPage;
