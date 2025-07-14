26c4a976476e48e3bb50f17e443659ff9d4d9f4d
HarenKei <seungmin1266@gmail.com>
2025-07-14 15:10:20 +0900

    Refactor: Refactoring CommonUserEntiity
    변경된 유저 테이블의 형식에 맞게 CommonUserEntity를 수정하였습니다.

--- backend/users/signup/applications/usecases/SignUpUseCase.ts
+++ backend/users/signup/applications/usecases/SignUpUseCase.ts
@@ -12,7 +12,12 @@ export class SignUpUsecase {
 
   async execute(dto: SignUpDto) {
     //여기에 비즈니스 로직 구현
+
     dto.uuid = uuidv4(); // UUID v4 문자열 생성
     return await this.repository.signUp(dto);
   }
+}
+
+function generateFourDigitRandom() {
+  return Math.floor(1000 + Math.random() * 9000);
 }
--- backend/users/user/domains/entities/CommonUserEntity.ts
+++ backend/users/user/domains/entities/CommonUserEntity.ts
@@ -1,31 +1,30 @@
 // 공통 사용자 엔티티
 export class CommonUserEntity {
   constructor(
-    public readonly id: number, // 사용자 고유 ID (수정 불가능)
-    public uuid: string, // 사용자 고유 UUID (수정 불가능)
-    public phoneNumber: string, // 전화번호
-    public password: string, // 비밀번호
-    public email: string, // 이메일
-    public age: number, // 나이
-    public profileImgUrl: string, // 프로필 이미지 URL
-    public address: string, // 주소
-    public name: string, // 이름
-    public readonly createdAt: Date // 생성일시 (수정 불가능)
+    public readonly id: string, // uuid
+    public phone_number: string,
+    public password: string,
+    public email: string,
+    public age: number,
+    public profile_img_url: string,
+    public address: string,
+    public name: string,
+    public nickname: string,
+    public readonly created_at: Date
   ) { }
 
-  // 엔티티를 객체로 변환하는 메서드
   toJSON() {
     return {
       id: this.id,
-      uuid: this.uuid,
-      phoneNumber: this.phoneNumber,
+      phone_number: this.phone_number,
       password: this.password,
       email: this.email,
       age: this.age,
-      profileImgUrl: this.profileImgUrl,
+      profile_img_url: this.profile_img_url,
       address: this.address,
       name: this.name,
-      createdAt: this.createdAt
+      nickname: this.nickname,
+      created_at: this.created_at
     };
   }
 } 
